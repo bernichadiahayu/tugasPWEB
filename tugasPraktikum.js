b5d1f6a0208e7d0b1c7409c5c75ce9f67b46f8b6
bernichadiahayu <bernichaayusafitri@gmail.com>
2020-12-10 06:50:12 +0700

    tugasPraktikum

---
+++ tugasPraktikum.js
@@ -0,0 +1,8 @@
+alert('hello beauty, welcome to our website!')
+
+function submitdata(){
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+
+	alert("email : " + email +" | " + "password : " + password)
+}
